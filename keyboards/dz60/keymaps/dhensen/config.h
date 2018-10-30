f38ea7de11c949a96290af321584e61f5b2a927a
Dino Hensen <dino.hensen@gmail.com>
2018-10-30 22:37:42 +0100

    added mouse movement, audio toggle,next,prev,volume up/down,right control, printscreen, insert

---
+++ keyboards/dz60/keymaps/dhensen/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_KEYMAP_H
+#define CONFIG_KEYMAP_H
+
+#include "../../config.h"
+
+#define MOUSEKEY_DELAY             50
+#define MOUSEKEY_INTERVAL          20
+#define MOUSEKEY_MAX_SPEED         8
+#define MOUSEKEY_TIME_TO_MAX       30
+#define MOUSEKEY_WHEEL_MAX_SPEED   8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+
+#endif
